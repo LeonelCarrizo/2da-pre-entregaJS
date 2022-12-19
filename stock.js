8fc81e56f55005f251be81a736fc8bec13c27f1f
leonel carrizo <leonelcarrizo940@gmail.com>
2022-12-19 20:10:52 -0300

    2da entregaJS

---
+++ stock.js
@@ -0,0 +1,62 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "zapatos",
+        tipo: "zapatos con tacos",
+        descripcion: "color negros",
+        precio: 1000,
+        talla: "36  ",
+        cantidad:0
+    },
+
+    {
+        id: 2,
+        nombre: "remera",
+        tipo: "remera",
+        descripcion: "color roja y negra",
+        precio: 300,
+        talla: "M",
+        cantidad:0
+    },
+
+    {
+        id: 3,
+        nombre: "jeans",
+        tipo: "jeans",
+        descripcion: "color gris",
+        precio: 500,
+        talla: "S",
+        cantidad:0
+    },
+
+    {
+        id: 4,
+        nombre: "reloj",
+        tipo: "reloj",
+        descripcion: "color azul",
+        precio: 2000,
+        talla: "xl",
+        cantidad:0
+    },
+
+    {
+        id: 5,
+        nombre: "lentes",
+        tipo: "lentes",
+        descripcion: "lentes negros",
+        precio: 500,
+        talla: "S",
+        cantidad:0
+    },
+
+    {
+        id: 6,
+        nombre: "zapatilla",
+        tipo: "zapatilla",
+        descripcion: "color verde y rojo",
+        precio: 1500,
+        talla: "43",
+        cantidad:0
+    }
+]
